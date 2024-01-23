9841077476209b04b4015817c84e3d51ff155d53
BeatACVR <75857504+BeatACVR@users.noreply.github.com>
2024-01-22 18:21:27 -0600

    v1.0.2 update
    platformer sounds option and 'i love gd cologne' sound (added with permission from gd colon)

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,33 @@ class $modify(PlayerObject) {
 public:
   TodoReturn pushButton(PlayerButton p0) {
     PlayerObject::pushButton(p0);
-   if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
-        if (p0 != PlayerButton::Jump) {
-          return; //
-        }
-     }
+
+  if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
+      if (p0 != PlayerButton::Jump) {
+        return; //
+      }
+    }
+
     if (!GameManager::sharedState()->getPlayLayer())
       return;
 
-                        // get value of slider
-                        auto sliderValue = Mod::get()->getSettingValue<int64_t>("clicksound-currentsound");
+    // get value of slider
+    auto sliderValue = Mod::get()->getSettingValue<int64_t>("clicksound-currentsound");
     
-                        // use slider value to change 
-                        std::string soundInUse;
-                        if (sliderValue == 1) {
-                            soundInUse = "osu-hit.mp3"_spr;
-                        } else if (sliderValue == 2) {
-                            soundInUse = "vine-boom.mp3"_spr;
-                        } else if (sliderValue == 3) {
-                            soundInUse = "bwomp.mp3"_spr;
-                        } else if (sliderValue == 4) {
-                            soundInUse = "fire-in-the-hole.mp3"_spr;
-                        }
+    // use slider value to change 
+    std::string soundInUse;
+    // <-------------------------------- Add sounds here!
+    if (sliderValue == 1) {
+        soundInUse = "osu-hit.mp3"_spr;
+    } else if (sliderValue == 2) {
+        soundInUse = "vine-boom.mp3"_spr;
+    } else if (sliderValue == 3) {
+        soundInUse = "bwomp.mp3"_spr;
+    } else if (sliderValue == 4) {
+        soundInUse = "fire-in-the-hole.mp3"_spr;
+    } else if (sliderValue == 5) {
+        soundInUse = "i-love-gd-cologne.mp3"_spr;
+    }
 
     // play sound from slider value pick sound
     FMODAudioEngine::sharedEngine()->playEffect(soundInUse);
