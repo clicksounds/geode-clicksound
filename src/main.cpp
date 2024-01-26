df9684498cb17e8d22251adcdd0a1d9d3be59be9
BeatACVR <75857504+BeatACVR@users.noreply.github.com>
2024-01-25 21:10:17 -0600

    v1.1.2 update

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ using namespace geode::prelude;
 class $modify(PlayerObject) {
 
 public:
+  
+  // CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS CLICK SOUNDS
+
   TodoReturn pushButton(PlayerButton p0) {
     PlayerObject::pushButton(p0);
 
@@ -18,48 +21,92 @@ public:
     if (!GameManager::sharedState()->getPlayLayer())
       return;
 
-    auto sliderValue = Mod::get()->getSettingValue<int64_t>("clicksound-currentsound");
-    auto customSound = Mod::get()->getSettingValue<ghc::filesystem::path>("custom-sound").string();
-    bool usingCustomSound;
-    std::string soundInUse;
+    auto clickSliderValue = Mod::get()->getSettingValue<int64_t>("clicksound-currentsound");
+    auto customClickSound = Mod::get()->getSettingValue<ghc::filesystem::path>("custom-clicksound").string();
+    bool usingCustomClickSound;
+    std::string clickSoundInUse;
 
-    if (sliderValue != 0) {
-      usingCustomSound = false;
-      if (sliderValue == 1) {
-        soundInUse = "osu-hit.ogg"_spr;
-      } else if (sliderValue == 2) {
-        soundInUse = "metronome.ogg"_spr;
-      } else if (sliderValue == 3) {
-        soundInUse = "click1.ogg"_spr;
-      } else if (sliderValue == 4) {
-        soundInUse = "click2.ogg"_spr;
-      } else if (sliderValue == 5) {
-        soundInUse = "fire-in-the-hole.ogg"_spr;
-      } else if (sliderValue == 6) {
-        soundInUse = "i-love-gd-cologne.ogg"_spr;
-      } else if (sliderValue == 7) {
-        soundInUse = "vine-boom.ogg"_spr;
-      } else if (sliderValue == 8) {
-        soundInUse = "bwomp.ogg"_spr;
-      } else if (sliderValue == 9) {
-        soundInUse = "metal-pipe.ogg"_spr;
-      } else if (sliderValue == 10) {
-        soundInUse = "tiktok.ogg"_spr;
-      } else if (sliderValue == 11) {
-        soundInUse = "plug.ogg"_spr;
+    if (clickSliderValue != 0) {
+      usingCustomClickSound = false;
+      if (clickSliderValue == 1) {
+        clickSoundInUse = "osu-hit.ogg"_spr;
+      } else if (clickSliderValue == 2) {
+        clickSoundInUse = "metronome.ogg"_spr;
+      } else if (clickSliderValue == 3) {
+        clickSoundInUse = "click1.ogg"_spr;
+      } else if (clickSliderValue == 4) {
+        clickSoundInUse = "click2.ogg"_spr;
+      } else if (clickSliderValue == 5) {
+        clickSoundInUse = "click3.ogg"_spr;
+      } else if (clickSliderValue == 6) {
+        clickSoundInUse = "fire-in-the-hole.ogg"_spr;
+      } else if (clickSliderValue == 7) {
+        clickSoundInUse = "i-love-gd-cologne.ogg"_spr;
+      } else if (clickSliderValue == 8) {
+        clickSoundInUse = "vine-boom.ogg"_spr;
+      } else if (clickSliderValue == 9) {
+        clickSoundInUse = "bwomp.ogg"_spr;
+      } else if (clickSliderValue == 10) {
+        clickSoundInUse = "metal-pipe.ogg"_spr;
+      } else if (clickSliderValue == 11) {
+        clickSoundInUse = "tiktok.ogg"_spr;
+      } else if (clickSliderValue == 12) {
+        clickSoundInUse = "plug.ogg"_spr;
+      } else if (clickSliderValue == 13) {
+        clickSoundInUse = "el-gato.ogg"_spr;
       }
     } 
     
-    if (sliderValue == 0) {
-      usingCustomSound = true;
+    if (clickSliderValue == 0) {
+      usingCustomClickSound = true;
     }
 
-    if (Mod::get()->getSettingValue<bool>("enable-clicksound") && !usingCustomSound) {
-        FMODAudioEngine::sharedEngine()->playEffect(soundInUse);
+    if (Mod::get()->getSettingValue<bool>("enable-clicksound") && !usingCustomClickSound) {
+        FMODAudioEngine::sharedEngine()->playEffect(clickSoundInUse);
     } 
     
-    if (Mod::get()->getSettingValue<bool>("enable-clicksound") && usingCustomSound) {
-        FMODAudioEngine::sharedEngine()->playEffect(customSound);
+    if (Mod::get()->getSettingValue<bool>("enable-clicksound") && usingCustomClickSound) {
+        FMODAudioEngine::sharedEngine()->playEffect(customClickSound);
     }
   }
+  
+  // RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS RELEASE SOUNDS 
+  
+ TodoReturn releaseButton(PlayerButton p0) {
+    PlayerObject::releaseButton(p0);
+
+  if (Mod::get()->getSettingValue<bool>("OnlyOnJump")) {
+      if (p0 != PlayerButton::Jump) {
+        return;
+      }
+    }
+
+    if (!GameManager::sharedState()->getPlayLayer())
+      return;
+
+    auto releaseSliderValue = Mod::get()->getSettingValue<int64_t>("releasesound-currentsound");
+    auto customReleaseSound = Mod::get()->getSettingValue<ghc::filesystem::path>("custom-releasesound").string();
+    bool usingCustomReleaseSound;
+    std::string releaseSoundInUse;
+
+    if (releaseSliderValue != 0) {
+      usingCustomReleaseSound = false;
+      if (releaseSliderValue == 1) {
+        releaseSoundInUse = "release1.ogg"_spr;
+      }
+    } 
+    
+    if (releaseSliderValue == 0) {
+      usingCustomReleaseSound = true;
+    }
+
+    if (Mod::get()->getSettingValue<bool>("enable-releasesound") && !usingCustomReleaseSound) {
+        FMODAudioEngine::sharedEngine()->playEffect(releaseSoundInUse);
+    } 
+    
+    if (Mod::get()->getSettingValue<bool>("enable-releasesound") && usingCustomReleaseSound) {
+        FMODAudioEngine::sharedEngine()->playEffect(customReleaseSound);
+    }
+  }
+
 };
