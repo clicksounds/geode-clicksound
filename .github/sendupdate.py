1c5a80d819dd9cbf6aa19fd5a9ac5a96d836e504
Coop <cooptheloop8@gmail.com>
2024-02-04 22:27:02 -0600

    Update sendupdate.py

--- .github/sendupdate.py
+++ .github/sendupdate.py
@@ -20,17 +20,17 @@ def send_webhook(eee):
 	import os
 	eeee = []
 	for eeeee in eee:
-		print(eeeee.decode('utf-8'))
-		eeee.append(eeeee.decode('utf-8') + "\n")
-	print(eeee)
+		# print(eeeee.decode('utf-8'))
+		eeee.append(eeeee.decode('utf-8'))
+	# print(eeee)
 	eeeeee = "".join(eeee)
-	print(eeeeee)
+	# print(eeeeee)
   
 	req = request.Request(os.getenv('DISCORD_WEBHOOK_URL'), method='POST')
 	req.add_header('User-Agent', 'python urllib')
 	req.add_header('Content-Type', 'application/json')
 	data = {
-		'content': "#" + eeeeee.split("##")[1],
+		'content': ("#" + eeeeee.split("##")[1]).replace(os.getenv('TAG').split("-")[1], "Click Sounds " + os.getenv('TAG').split("-")[1] + " is out on Github!"),
 	}
 	request.urlopen(req, data=json.dumps(data).encode('utf-8'))
 
