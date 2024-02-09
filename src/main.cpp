bfabc9cdea1a2d90ad0d62f4bc2b7bcd8c0485a8
Weebify <weebifylmao123@gmail.com>
2024-02-09 11:17:21 +0700

    uhhhh

--- src/main.cpp
+++ src/main.cpp
@@ -185,18 +185,18 @@ public:
 // i was getting mad at the function, (i'm bi i just think it's funni - viper)
 class $modify(YoureAPansexual,PauseLayer) {
   void YOUAREGAY(CCObject*) {
-  		geode::openSettingsPopup(Mod::get());
+      geode::openSettingsPopup(Mod::get());
   }
 
   // fuck yall i fixed the indentation for this snippet
   void customSetup() {
-  	PauseLayer::customSetup();
+    PauseLayer::customSetup();
 
-  	auto winSize = CCDirector::sharedDirector()->getWinSize();
+    auto winSize = CCDirector::sharedDirector()->getWinSize();
 
     auto menu = this->getChildByID("left-button-menu");
 
-  	auto spr = CCSprite::create("Button.png"_spr);
+    auto spr = CCSprite::create("Button.png"_spr);
 
     if(Mod::get()->getSettingValue<bool>("settings-button")) {
       spr->setScale(0.7f);
@@ -218,56 +218,56 @@ class $modify(YoureAPansexual,PauseLayer) {
 
 // Developer badges! :3333 (ty kot!)
 class buttonfunctionthing : public CCLayer {
-	public:
-	void onButtonClick(CCObject* sender) {
-		FLAlertLayer::create(
-			"About",
-			"This badge is given to <cp>Click Sound developers</c>. If you have programmed anything that was added to Click Sound, You will obtain this badge. ^w^",
-			"OK"
-		)->show();
-	}
+  public:
+  void onButtonClick(CCObject* sender) {
+    FLAlertLayer::create(
+      "About",
+      "This badge is given to <cp>Click Sound developers</c>. If you have programmed anything that was added to Click Sound, You will obtain this badge. ^w^",
+      "OK"
+    )->show();
+  }
 };
 
 class $modify(ProfilePage) {
-	bool m_hasBeenOpened = false;
+  bool m_hasBeenOpened = false;
 
-	bool init(int p0, bool p1) {
-		if (!ProfilePage::init(p0, p1)) return false;
+  bool init(int p0, bool p1) {
+    if (!ProfilePage::init(p0, p1)) return false;
 
-		std::set<int> csContributors = {13873867, 14284530, 21207551, 25397826};
-																									
+    std::set<int> csContributors = {13873867, 14284530, 21207551, 25397826};
+                                                  
 
-		m_mainLayer->removeChildByID("my-stuff-hint");
-		auto mainMenu = static_cast<cocos2d::CCLayer*>(this->getChildByIDRecursive("main-menu"));
-		auto spr = CCSprite::create("csBadge.png"_spr);
-		auto btn = CCMenuItemSpriteExtra::create(
-       		spr, this, menu_selector(buttonfunctionthing::onButtonClick)
-    	);
-		auto contentSize = mainMenu->getContentSize();
+    m_mainLayer->removeChildByID("my-stuff-hint");
+    auto mainMenu = static_cast<cocos2d::CCLayer*>(this->getChildByIDRecursive("main-menu"));
+    auto spr = CCSprite::create("csBadge.png"_spr);
+    auto btn = CCMenuItemSpriteExtra::create(
+           spr, this, menu_selector(buttonfunctionthing::onButtonClick)
+      );
+    auto contentSize = mainMenu->getContentSize();
 
 
-		btn->setPosition(14.500, -132);
-		btn->setZOrder(26);
-		btn->setID("cs-badge");
-		mainMenu->addChild(btn);
-		btn->setVisible(false);
+    btn->setPosition(14.500, -132);
+    btn->setZOrder(26);
+    btn->setID("cs-badge");
+    mainMenu->addChild(btn);
+    btn->setVisible(false);
 
-		std::string username = m_usernameLabel->getString();
+    std::string username = m_usernameLabel->getString();
 
-		if (csContributors.count(p0) > 0) 
-		{
-			btn->setVisible(true);
+    if (csContributors.count(p0) > 0) 
+    {
+      btn->setVisible(true);
         }
-		else
-		{
-			// how many times do we have to tell you this old man
-		}
+    else
+    {
+      // how many times do we have to tell you this old man
+    }
 
-		auto posX = m_usernameLabel->getPositionX();
-		auto posY = m_usernameLabel->getPositionY();
+    auto posX = m_usernameLabel->getPositionX();
+    auto posY = m_usernameLabel->getPositionY();
 
 
-		return true;
-	}
+    return true;
+  }
 };
 class $modify(EndLevelLayer){void customSetup(){EndLevelLayer::customSetup();if(carrot==true){auto eee = CCNode::create();auto ee = CCSprite::create("ee.png"_spr);eee->setPosition(450, 260);eee->setAnchorPoint({0.5, 0.5});eee->setScale(0.2);ee->setOpacity(10);eee->addChild(ee);static_cast<cocos2d::CCLayer*>(this->getChildren()->objectAtIndex(0))->addChild(eee);}carrot=false;}};
