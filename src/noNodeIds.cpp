1e875f54492d178d1e8f263e434b90be6518192a
MuhXd <momo2010sofia@gmail.com>
2024-02-17 22:52:00 +0000

    geode index!

--- src/noNodeIds.cpp
+++ src/noNodeIds.cpp
@@ -7,7 +7,7 @@ using namespace geode::prelude;
 // test disabling the menulayer
 class $modify(MenuLayer) { 
 static void onModify(auto& self) {
-        self.setHookPriority("MenuLayer::init", 10000000); // GO FIRST :O
+        self.setHookPriority("MenuLayer::init", -100); // GO FIRST :O
     }
      bool init() {
         if (!MenuLayer::init())
@@ -18,13 +18,27 @@ static void onModify(auto& self) {
         };
 
            auto alert = FLAlertLayer::create(
-             "Blud Doesn't have NodeIDs",
+             "Click Sounds Error",
              "Please install node ids from <cp>geode index!</c> ",  
               "OK"
             );
             alert->m_scene = this;
+           
+            for(auto items : CCArrayExt<CCNode*>(this->getChildren())) {
+                items->setVisible(false)
+            };
+             for(auto items : CCArrayExt<CCNode*>(this->getChildByID("bottom-menu")->getChildren())) {
+                items->setVisible(false)
+            };
+            this->getChildByID("bottom-menu")->setVisible(true);
+             this->getChildByID("close-menu")->setVisible(true);
+            this->getChildByID("bottom-menu")->setScale(2.6);
+             this->getChildByID("bottom-menu")->setLayout(
+                RowLayout::create()
+                ->setGap(-51)
+             );
+            this->getChildByID("bottom-menu")->getChildByID("geode.loader/geode-button")->setVisible(true);
             alert->show();
-        
         return true;
      };
 };
