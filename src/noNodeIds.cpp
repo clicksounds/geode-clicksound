8fe07ef230a407408638822e00a0bf99fbf77198
Coop <73203995+coopeeo@users.noreply.github.com>
2024-02-18 00:21:41 -0600

    Update noNodeIds.cpp

--- src/noNodeIds.cpp
+++ src/noNodeIds.cpp
@@ -3,16 +3,23 @@
 #include <Geode/loader/Mod.hpp>
 #include <Geode/loader/Loader.hpp>
 #include <Geode/ui/GeodeUI.hpp>
+#include <Geode/loader/Index.hpp>
 
 using namespace geode::prelude;
+auto leBtn = nullptr;
+auto leRestartBtn = nullptr;
+
 // test disabling the menulayer
-class $modify(newl,MenuLayer) { 
+class $modify(newl,newll,MenuLayer) { 
     void index(CCObject*) {
         auto nodeIDSmod = geode::Index::get()->getItemsByModID("geode.node-ids").back();
         auto nodeIdsMetadata = nodeIDSmod->getMetadata();
         Mod theNodeIds = Mod(nodeIdsMetadata);
         Mod* theNodeId2 = &theNodeIds;
         geode::openIndexPopup(theNodeId2);
+    };
+    void index2(CCObject*) {
+        utils::game::restart();
     };
 
     void initUi() {
@@ -23,6 +30,21 @@ class $modify(newl,MenuLayer) {
             spr, this, menu_selector(newl::index)
         );
         btn->setScale(1.7);
+        btn->setPosition(winSize.width / 2, winSize.height / 2);
+        leBtn = btn;
+        this->getChildByID("bottom-menu")->addChild(btn);
+    };
+    void initUi2() {
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        // auto spr = ButtonSprite::create("/nodeIdsLogo.png"_spr);
+        auto spr = ButtonSprite::create("Restart Game");
+        auto btn = CCMenuItemSpriteExtra::create(
+            spr, this, menu_selector(newll::index2)
+        );
+        btn->setScale(1.7);
+        btn->setPosition(winSize.width / 2, winSize.height / 2);
+        btn->setVisible(false);
+        leRestartBtn = btn;
         this->getChildByID("bottom-menu")->addChild(btn);
     };
 
@@ -64,8 +86,14 @@ class $modify(newl,MenuLayer) {
         this->getChildByID("bottom-menu")->setVisible(false);
         alert->show();
         newl::initUi();
+        newll::initUi2();
            
                
         return true;
     };
 };
+
+auto listener = EventListener<ModInstallFilter>(+[](ModInstallEvent* ev) {
+    leBtn->setVisible(false);
+    leRestartBtn->setVisible(true);
+}, ModInstallFilter("geode.node-ids"));
