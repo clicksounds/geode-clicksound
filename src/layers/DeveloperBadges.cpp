585988c4f5bbb80b582632bbc446e387c0087879
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-02-24 20:16:38 -0600

    Upload all of this

--- src/layers/DeveloperBadges.cpp
+++ src/layers/DeveloperBadges.cpp
@@ -1,22 +1,11 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/PlayerObject.hpp>
-#include <Geode/modify/PauseLayer.hpp>
-#include <Geode/modify/EndLevelLayer.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 #include <Geode/modify/ProfilePage.hpp>
 #include <Geode/binding/ProfilePage.hpp>
 #include <Geode/Bindings.hpp>
 #include <Geode/loader/Loader.hpp>
 
-#include "./utils/ee.hpp"
-
-#include <algorithm>
-#include <cctype>
-
 using namespace geode::prelude;
-// 🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕
-
-bool Carrot::carrot = false;
 
 // Developer badges! :3333 (ty kot!)
 class buttonfunctionthing : public CCLayer {
@@ -68,4 +57,3 @@ class $modify(ProfilePage) {
     return true;
   }
 };
-class $modify(EndLevelLayer){void customSetup(){EndLevelLayer::customSetup();if(Carrot::carrot==true){auto eee = CCNode::create();auto ee = CCSprite::create("ee.png"_spr);eee->setPosition(450, 260);eee->setAnchorPoint({0.5, 0.5});eee->setScale(0.2);ee->setOpacity(10);eee->addChild(ee);static_cast<cocos2d::CCLayer*>(this->getChildren()->objectAtIndex(0))->addChild(eee);}Carrot::carrot=false;}};
