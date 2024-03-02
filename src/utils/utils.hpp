3ba862ecb94502bfd3b9116bf4bde7bb89e05cb2
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-02 16:59:23 -0600

    iofdsapofdsihyfpdsafudesafidusfdisoapfudsiaopfudsoaipfydsopafhdsiupafdsa

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -10,18 +10,18 @@
 using namespace geode::prelude;
 
 Mod* newIndexToMod(std::string url) {
-    Mod mod;
+    auto& metadata = ModMetadata::create(matjson::parse("{}")).unwrap();
     web::AsyncWebRequest()
         .fetch(url)
 	    .json()
         .then([&](auto const& webRes){
             auto res = webRes["payload"];
-            auto& metadata = ModMetadata::create(matjson::parse("{}")).unwrap();
             metadata->setVersion(res["versions"][0]["name"]);
             metadata->setName(res["versions"][0]["version"]);
             metadata->setDevelopers({res["developers"][0]["display_name"]});
             metadata->setDescription(res["versions"][0]["description"]);
-            mod = Mod(metadata);
 	    });
-    return &mod;
+    Mod theNodeIds = Mod(metadata);
+    Mod* theNodeId2 = &mtheNodeIdsod;
+    return theNodeId2;
 }
