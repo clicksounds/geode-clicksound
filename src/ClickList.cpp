34e2bb2337a35c572c6aeb1ecf593dedf41b8eee
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-11 18:12:58 +0000

    Easier to modify and read

--- src/ClickList.cpp
+++ src/ClickList.cpp
@@ -1,11 +1,24 @@
 #include "./utils/Clicks.hpp"
+#include <string>
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+static const char* convertToConstChar(const std::string& text) {
+    static std::string str;
+    str = text;
+    return str.c_str();
+}
+static std::string getmodfix() {
+  return ""_spr;
+}
 
 const char* Clicks::getClickSprite(int clickSliderValue) {
     const char* clickSoundInUse;
     switch(clickSliderValue) {
       // Other
       case 0: clickSoundInUse = "__USECUSTOM__"; break;
-
+    // convertToConstChar(fmt::format("{}Name-{}.ogg",getmodfix(),std::to_string((rand() % HowMany) + 1 )));
       // Useful sounds
       case 1: clickSoundInUse = "osu-hit.ogg"_spr; break;
       case 2: clickSoundInUse = "metronome.ogg"_spr; break;
@@ -18,25 +31,25 @@ const char* Clicks::getClickSprite(int clickSliderValue) {
       case 9: clickSoundInUse = "spacebar1.ogg"_spr; break;
       case 10: clickSoundInUse = "uparrow2.ogg"_spr; break;
       case 11: clickSoundInUse = "clickrelease-4-click.ogg"_spr; break;
-      case 12: clickSoundInUse = (rand() % 5 == 0) ? "faze-click-1.ogg"_spr : ((rand() % 5 == 1) ? "faze-click-2.ogg"_spr : ((rand() % 5 == 2) ? "faze-click-3.ogg"_spr : ((rand() % 5 == 3) ? "faze-click-4.ogg"_spr : "faze-click-5.ogg"_spr))); break;
-      case 13: clickSoundInUse = "uparrow3.ogg"_spr; break;
-      case 14: clickSoundInUse = (rand() % 5 == 0) ? "bloody-click-1.ogg"_spr : ((rand() % 5 == 1) ? "bloody-click-2.ogg"_spr : ((rand() % 5 == 2) ? "bloody-click-3.ogg"_spr : ((rand() % 5 == 3) ? "bloody-click-4.ogg"_spr : "bloody-click-5.ogg"_spr))); break;
+      case 12: clickSoundInUse = convertToConstChar(fmt::format("{}faze-click-{}.ogg",getmodfix(),std::to_string((rand() % 5) + 1 ))); break; 
+      case 13: clickSoundInUse = "uparrow3.ogg"_spr; break; 
+      case 14: clickSoundInUse = convertToConstChar(fmt::format("{}bloody-click-{}.ogg",getmodfix(),std::to_string((rand() % 5) + 1 ))); break;
       case 15: clickSoundInUse = "spaceuk.ogg"_spr; break;
       case 16: clickSoundInUse = "npesta-click.ogg"_spr; break;
       case 17: clickSoundInUse = "cursed.ogg"_spr; break;
       case 18: clickSoundInUse = "spacebar2-click.ogg"_spr; break;
-      case 19: clickSoundInUse = "click3.ogg"_spr; break;        
-      case 20: clickSoundInUse = ((rand() % 5 == 0) ? "Viper_Clicks_SpaceBar_1.ogg"_spr : ((rand() % 5 == 1) ? "Viper_Clicks_SpaceBar_2.ogg"_spr : ((rand() % 5 == 2) ? "Viper_Clicks_SpaceBar_3.ogg"_spr : ((rand() % 5 == 3) ? "Viper_Clicks_SpaceBar_4.ogg"_spr : "Viper_Clicks_SpaceBar_5.ogg"_spr)))); break;
-      case 21: clickSoundInUse = ((rand() % 5 == 0) ? "logitech_mx_anywhere_2s_Clicks_1.ogg"_spr : ((rand() % 5 == 1) ? "logitech_mx_anywhere_2s_Clicks_2.ogg"_spr : ((rand() % 5 == 2) ? "logitech_mx_anywhere_2s_Clicks_3.ogg"_spr : ((rand() % 5 == 3) ? "logitech_mx_anywhere_2s_Clicks_4.ogg"_spr : "logitech_mx_anywhere_2s_Clicks_5.ogg"_spr)))); break;
+      case 19: clickSoundInUse = "click3.ogg"_spr; break;  
+      case 20: clickSoundInUse = convertToConstChar(fmt::format("{}Viper_Clicks_SpaceBar_{}.ogg",getmodfix(),std::to_string((rand() % 5) + 1 ))); break;
+      case 21: clickSoundInUse = convertToConstChar(fmt::format("{}logitech_mx_anywhere_2s_Clicks_{}.ogg",getmodfix(),std::to_string((rand() % 5) + 1 ))); break;
       case 22: clickSoundInUse = "zoink-click.ogg"_spr; break;
       case 23: clickSoundInUse = "screen-tap-1.ogg"_spr; break;
       case 24: clickSoundInUse = "plate.ogg"_spr; break;
       case 25: clickSoundInUse = "neiro-click.ogg"_spr; break;
       case 26: clickSoundInUse = "doggie.ogg"_spr; break;
       case 27: clickSoundInUse = "trusta.ogg"_spr; break;
-      case 28: clickSoundInUse = "gazonk.ogg"_spr; break;
-      case 29: clickSoundInUse = ((rand() % 8 == 0) ? "sayo-click-1.ogg"_spr : ((rand() % 8 == 1) ? "sayo-click-2.ogg"_spr : ((rand() % 8 == 2) ? "sayo-click-3.ogg"_spr : ((rand() % 8 == 3) ? "sayo-click-4.ogg"_spr : ((rand() % 8 == 4) ? "sayo-click-5.ogg"_spr : ((rand() % 8 == 5) ? "sayo-click-6.ogg"_spr : ((rand() % 8 == 6) ? "sayo-click-7.ogg"_spr : "sayo-click-8.ogg"_spr))))))); break;
-      case 30: clickSoundInUse = ((rand() % 9 == 0) ? "viper-click-1.ogg"_spr : ((rand() % 9 == 1) ? "viper-click-2.ogg"_spr : ((rand() % 9 == 2) ? "viper-click-3.ogg"_spr : ((rand() % 9 == 3) ? "viper-click-4.ogg"_spr : ((rand() % 9 == 4) ? "viper-click-5.ogg"_spr : ((rand() % 9 == 5) ? "viper-click-6.ogg"_spr : ((rand() % 9 == 6) ? "viper-click-7.ogg"_spr : ((rand() % 9 == 7) ? "viper-click-8.ogg"_spr : "viper-click-9.ogg"_spr)))))))); break;
+      case 28: clickSoundInUse = "gazonk.ogg"_spr; break; 
+      case 29: clickSoundInUse = convertToConstChar(fmt::format("{}sayo-click-{}.ogg",getmodfix(),std::to_string((rand() % 8) + 1 ))); break;
+      case 30: clickSoundInUse = convertToConstChar(fmt::format("{}viper-click-{}.ogg",getmodfix(),std::to_string((rand() % 9) + 1 ))); break;
 
 
       // Meme sounds
