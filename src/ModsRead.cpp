01a48c3ec3520b5b1efe7a667fe0ad2ee3997020
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-11 18:36:18 +0000

    Update ModsRead.cpp

--- src/ModsRead.cpp
+++ src/ModsRead.cpp
@@ -1,6 +1,10 @@
 #include <Geode/binding/GJGameState.hpp>
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
+class GJGameState
+{
+static int CurentClicksound = 0;
+};
 
 $on_mod(Loaded) {
 listenForSettingChanges("clicksound-currentsound", +[](int value) {
