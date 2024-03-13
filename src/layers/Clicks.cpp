f9273310678181bc4d3bdecd9bdaa3505ab27c71
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-12 19:18:48 -0500

    safdsafsadfaf

--- src/layers/Clicks.cpp
+++ src/layers/Clicks.cpp
@@ -23,7 +23,7 @@ public:
     
     if (!GameManager::sharedState()->getPlayLayer() && !GameManager::sharedState()->getEditorLayer()) return;
     if (PlayLayer::get()) if (this == PlayLayer::get()->m_player2 && !PlayLayer::get()->m_level->m_twoPlayerMode) return;
-    if (LevelEditorLayer::get()) {log::debug('{}', LevelEditorLayer::get()->m_level->m_twoPlayerMode);if (this == LevelEditorLayer::get()->m_player2 && !LevelEditorLayer::get()->m_level->m_twoPlayerMode) return;}
+    if (LevelEditorLayer::get()) {log::debug("{}", LevelEditorLayer::get()->m_level->m_twoPlayerMode);if (this == LevelEditorLayer::get()->m_player2 && !LevelEditorLayer::get()->m_level->m_twoPlayerMode) return;}
     
     auto clickSliderValue = Mod::get()->getSettingValue<int64_t>("clicksound-currentsound");
     auto customClickSound = Mod::get()->getSettingValue<ghc::filesystem::path>("custom-clicksound").string();
