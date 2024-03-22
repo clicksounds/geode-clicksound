0216e25d0bdab25449bbe3e3a6ba4f4aca9f0c11
Weebifying <weebifylmao123@gmail.com>
2024-03-22 10:26:22 +0700

    classes for types value

---
+++ src/utils/AnotherSettings.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <Geode/loader/SettingNode.hpp>
+using namespace geode::prelude;
+
+class ClickTypeValue : public SettingValue {
+protected:
+    int m_type;
+public:
+    ClickTypeValue(std::string const& key, std::string const& modID, int type)
+      : SettingValue(key, modID), m_type(type) {}
+
+    bool load(matjson::Value const& json) override {
+        if (!json.is<int>()) return false;
+        m_type = json.as<int>();
+        return true;
+    }
+
+    bool save(matjson::Value& json) const override {
+        json = m_type;
+        return true;
+    }
+
+    SettingNode* createNode(float width) override;
+
+    void setType(int type) {
+        m_type = type;
+    }
+
+    int getType() const {
+        return m_type;
+    }
+}
+
+class ReleaseTypeValue : public SettingValue {
+protected:
+    int m_type;
+public:
+    ReleaseTypeValue(std::string const& key, std::string const& modID, int type)
+      : SettingValue(key, modID), m_type(type) {}
+
+    bool load(matjson::Value const& json) override {
+        if (!json.is<int>()) return false;
+        m_type = json.as<int>();
+        return true;
+    }
+
+    bool save(matjson::Value& json) const override {
+        json = m_type;
+        return true;
+    }
+
+    SettingNode* createNode(float width) override;
+
+    void setType(int type) {
+        m_type = type;
+    }
+
+    int getType() const {
+        return m_type;
+    }
+}
