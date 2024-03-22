ac3ebec062d1e93bd7565caab687e8f07cf9ab7d
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-22 18:11:33 -0500

    okay pls

--- src/settings/ClickTypeNodes.hpp
+++ src/settings/ClickTypeNodes.hpp
@@ -7,18 +7,18 @@ using namespace geode::prelude;
 CCNode* createCheckboxButtonTexture(std::string text, float width, bool isSelect){
     auto label = CCLabelBMFont::create(text.c_str(), "chatFont.fnt");
     label->setScale(.33F);
-    label->setPosition((width/2)/2, 18);
+    label->setPosition(((width/2)/2)/3, 18);
     auto bgSelector = cocos2d::extension::CCScale9Sprite::create(
-            "square02b_001.png", { 0.0f, 0.0f, 70.0f, 80.0f }
+            "square02b_small.png", { 0.0f, 0.0f, 40.0f, 40.0f }
         );
     bgSelector->setColor({ 255, 255, 255 });
     if (isSelect) {bgSelector->setOpacity(210);} else {bgSelector->setOpacity(0);}
-    bgSelector->setContentSize({ (width/2)/3, 32.f });
-    bgSelector->setPosition((width/2)/3, 0);
+    bgSelector->setContentSize({ ((width/2)/2)/3, 32.f });
+    bgSelector->setPosition(((width/2)/2)/3, 0);
     auto layer = CCLayer::create();
     layer->addChild(label);
     layer->addChild(bgSelector);
-    layer->setContentSize({ (width/2)/3, 32.f });
+    layer->setContentSize({ ((width/2)/2)/3, 32.f });
     //layer->setPosition((width/2)/2, 18);
     return static_cast<CCNode*>(layer);
 }
