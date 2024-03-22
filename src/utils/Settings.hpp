d220a7042dc25e23510c3ed76db3791707dd163a
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-22 11:39:39 -0500

    lets go

--- src/utils/Settings.hpp
+++ src/utils/Settings.hpp
@@ -6,8 +6,8 @@ using namespace geode::prelude;
 
 
 CCNode* createCheckboxButtonTexture(std::string text, bool isSelect){
-    auto label = CCLabelBMFont::create(text, "chatFont.fnt");
-    if (isSelect){label->setString(fmt::format("{} + on", text));}
+    auto label = CCLabelBMFont::create(text.c_str(), "chatFont.fnt");
+    if (isSelect){label->setString(fmt::format("{} + on", text).c_str());}
     label->setScale(.33F);
     label->setPositionX(0);
     return static_cast<CCNode*>(label);
