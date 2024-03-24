81aa6bab2e41308d542f7cf05e5589481a04d777
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-24 16:54:13 -0500

    PLEASE

--- src/layers/Clicks.cpp
+++ src/layers/Clicks.cpp
@@ -48,7 +48,7 @@ public:
     if (PlayLayer::get()) if (this == PlayLayer::get()->m_player2 && !PlayLayer::get()->m_level->m_twoPlayerMode) return;
     if (LevelEditorLayer::get()) {if (this == LevelEditorLayer::get()->m_player2 && !LevelEditorLayer::get()->m_level->m_twoPlayerMode) return;}
     
-    matjson::Value settings = Mod::get()->getSettingValue<matjson::Value>("clicksound-type");
+    matjson::Value settings = Mod::get()->getSettingValue<matjson::Value*>("clicksound-type");
     
     if (Mod::get()->getSettingValue<bool>("enable-clicksound") && settings["tab"] != 3) {
       auto fae = FMODAudioEngine::sharedEngine();
@@ -120,7 +120,7 @@ public:
     if (PlayLayer::get()) if (this == PlayLayer::get()->m_player2 && !PlayLayer::get()->m_level->m_twoPlayerMode) return;
     if (LevelEditorLayer::get()) if (this == LevelEditorLayer::get()->m_player2 && !LevelEditorLayer::get()->m_level->m_twoPlayerMode) return;
 
-    matjson::Value settings = Mod::get()->getSettingValue<matjson::Value>("releasesound-type");
+    matjson::Value settings = Mod::get()->getSettingValue<matjson::Value*>("releasesound-type");
  
     if (Mod::get()->getSettingValue<bool>("enable-releasesound") && settings["tab"] != 3) {
       auto fae = FMODAudioEngine::sharedEngine();
