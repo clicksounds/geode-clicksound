6ef05f896861ae2ee064bf38a26cf66aee383e39
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-24 16:42:24 -0500

    oops

--- src/layers/Clicks.cpp
+++ src/layers/Clicks.cpp
@@ -58,9 +58,9 @@ public:
       FMOD::Sound* sound;
       
       if (settings["tab"] == 1){
-        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getClickSprite(settings["click"])).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
+        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getClickSprite(settings["click"].as<int>())).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
       } else if (settings["tab"] == 2) {
-        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getClickSprite(settings["memeClick"])).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
+        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getClickSprite(settings["memeClick"].as<int>())).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
       }
 
 
@@ -130,9 +130,9 @@ public:
       FMOD::Sound* sound;
 
       if (settings["tab"] == 1){
-        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getRealseSprite(settings["click"])).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
+        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getRealseSprite(settings["click"]).as<int>()).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
       } else if (settings["tab"] == 2) {
-        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getReleaseSprite(settings["memeClick"])).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
+        system->createSound((Mod::get()->getResourcesDir().parent_path() / Clicks::getReleaseSprite(settings["memeClick"].as<int>())).string().c_str(), FMOD_DEFAULT, nullptr, &sound);
       }
       
       if (Mod::get()->getSettingValue<bool>("use-sfx-volume")) {
@@ -164,7 +164,7 @@ public:
           channel->setVolume(fae->m_sfxVolume*2.f);
         #else
           if (Mod::get()->getSettingValue<int64_t>("volume-slider") != 0) system->playSound(sound, nullptr, false, &channel);
-          channel->setVolume( (Mod::get()->getSettingValue<int64_t>("volume-slider")/50.f)* calculateVolumeMultiplier() ) ;
+          channel->setVolume((Mod::get()->getSettingValue<int64_t>("volume-slider")/50.f)* calculateVolumeMultiplier());
         #endif
       } else {
         if (Mod::get()->getSettingValue<int64_t>("volume-slider") != 0) system->playSound(sound, nullptr, false, &channel);
