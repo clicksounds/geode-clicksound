7d4200507b5e6c4e2c8a787c588935b9a17a5e11
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-26 17:21:00 -0500

    oops update this thingy again

--- .github/AddToClickList/main.py
+++ .github/AddToClickList/main.py
@@ -24,16 +24,6 @@ jsonshitForChecking = {
     }
 }
 
-def find_update_folder():
-    for root, dirs, files in os.walk("."):
-        if "Update" in dirs:
-            return os.path.join(root, "Update")
-    return None
-
-def sanitize_filename(filename):
-    # Remove any characters that are not letters, numbers, underscores, or periods
-    return ((''.join(c for c in filename if c.isalnum() or c in ['.'] or c in [' '] or c in ['_'] or c in ['-']))).replace(" ", "_")
-
 def rename_files(folder_path2):
     folder_path = shutil.copytree("Update", "Output")
     shutil.rmtree("Update")
@@ -61,23 +51,19 @@ def rename_files(folder_path2):
                     o["fileCount"] += 1
 
 if __name__ == "__main__":
-    update_folder = find_update_folder()
-    if update_folder:
-        if os.path.exists("Output"):
-            shutil.rmtree("Output")
-        os.mkdir("Update")
-        shutil.copytree("../../../../clicks/Meme", "Update/Meme")
-        shutil.copytree("../../../../clicks/Useful", "Update/Useful")
-
-        rename_files(update_folder)
-
-        thingy = f'#pragma once\n#include <Geode/Geode.hpp>\n#include <matjson.hpp>\nusing namespace geode::prelude;\n\nauto jsonString = "{json.dumps(jsonshit)}";\n\nmatjson::Value getJson() {{\nreturn matjson::parse(jsonString)\n}}'
-        thingy2 = thingy.split("\n")
-        with open("../../../src/utils/getJSON.hpp", "w") as file:
-            for line in thingy2:
-                file.write(f'{line}\n')
-        
+    if os.path.exists("Output"):
         shutil.rmtree("Output")
-        print("Files renamed, converted, and original files removed successfully!")
-    else:
-        print("Update folder not found in the current directory.")
+    os.mkdir("Update")
+    shutil.copytree("../../../../clicks/Meme", "Update/Meme")
+    shutil.copytree("../../../../clicks/Useful", "Update/Useful")
+
+    rename_files(update_folder)
+
+    thingy = f'#pragma once\n#include <Geode/Geode.hpp>\n#include <matjson.hpp>\nusing namespace geode::prelude;\n\nauto jsonString = "{json.dumps(jsonshit)}";\n\nmatjson::Value getJson() {{\nreturn matjson::parse(jsonString)\n}}'
+    thingy2 = thingy.split("\n")
+    with open("../../../src/utils/getJSON.hpp", "w") as file:
+        for line in thingy2:
+            file.write(f'{line}\n')
+    
+    shutil.rmtree("Output")
+    print("Files renamed, converted, and original files removed successfully!")
