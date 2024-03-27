3c077486994b4aa72dd8ff02ec4c7181f9f9a42f
acaruso-xx <acaruso@tuta.io>
2024-03-27 12:34:31 -0400

    :construction: [Toolbox] Add sprite utils

--- src/click/Toolbox.cpp
+++ src/click/Toolbox.cpp
@@ -17,4 +17,26 @@ matjson::Value Toolbox::readJSON(std::string const& fileName) {
     return utils::file::readJson(path).unwrap();
 }
 
+CCSprite* Toolbox::createSprite(std::string const& name) {
+    auto* sprite = CCSprite::create();
+
+    if (sprite && initSprite(sprite, name)) {
+        return sprite;
+    }
+
+    return nullptr;
+}
+
+bool Toolbox::initSprite(CCSprite* sprite, std::string const& name) {
+    if (sprite->initWithFile(name.c_str())) {
+        return true;
+    }
+
+    if (sprite->initWithSpriteFrameName(name.c_str())) {
+        return true;
+    }
+
+    return false;
+}
+
 } // namespace click
--- src/click/Toolbox.hpp
+++ src/click/Toolbox.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cocos2d.h>
 #include <matjson.hpp>
 #include <string>
 
@@ -12,6 +13,16 @@ public:
     //! @param fileName File name.
     //! @return Parsed JSON data.
     static matjson::Value readJSON(std::string const& fileName);
+
+    //! @brief Create a sprite from either file or sprite frame.
+    //! @param name Image file name or sprite frame name.
+    //! @return Sprite.
+    static cocos2d::CCSprite* createSprite(std::string const& image);
+
+    //! @brief Initialize a sprite from either file or sprite frame.
+    //! @param name Image file name or sprite frame name.
+    //! @return Whether the initialization was successful.
+    static bool initSprite(cocos2d::CCSprite* sprite, std::string const& name);
 };
 
 } // namespace click
