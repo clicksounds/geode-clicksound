1ced118fc23ea5747bff0fb3e9280d959fd66b4f
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-26 21:27:48 -0500

    oops but BANG BANG BANG

--- src/settings/SettingsSetup.cpp
+++ src/settings/SettingsSetup.cpp
@@ -149,5 +149,5 @@ $on_mod(Loaded) {
     Mod::get()->addCustomSetting<ReleaseTypeValue>("releasesound-type", matjson::parse(R"({"tab":1,"click":1,"memeClick":1,"customClick":"..."})"));
     Mod::get()->addCustomSetting<SectionSettingValue>("releasesound-section", "none");
     Mod::get()->addCustomSetting<SectionSettingValue>("misc-section", "none");
-    Clicks::initJson()
+    Clicks::initJson();
 }
