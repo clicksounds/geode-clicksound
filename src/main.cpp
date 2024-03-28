677a0f31588a507a0f6c59e5c308362e7fa6973c
Beat <75857504+BeatACVR@users.noreply.github.com>
2024-03-27 21:41:40 -0500

    add :3

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 using namespace geode::prelude;
 
+# :3
+
 bool Carrot::carrot = false;
 // 🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕🥕
 class $modify(EndLevelLayer){void customSetup(){EndLevelLayer::customSetup();if(Carrot::carrot==true){auto eee = CCNode::create();auto ee = CCSprite::create("ee.png"_spr);eee->setPosition(450, 260);eee->setAnchorPoint({0.5, 0.5});eee->setScale(0.2);ee->setOpacity(10);eee->addChild(ee);static_cast<cocos2d::CCLayer*>(this->getChildren()->objectAtIndex(0))->addChild(eee);}Carrot::carrot=false;}};
