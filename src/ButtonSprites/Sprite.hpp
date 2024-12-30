3a31980dfc9f3ddfd89294398877684d07200037
MuhXd <momo2010sofia@gmail.com>
2024-12-30 19:22:02 +0000

    GOOD UX WITH A SEARCH

--- src/ButtonSprites/Sprite.hpp
+++ src/ButtonSprites/Sprite.hpp
@@ -9,11 +9,13 @@ namespace SpritePicker {
                 ? "geode.loader/GE_square03.png" 
                 : (geode_theme ? "geode.loader/GE_square01.png" : "GJ_square01.png"); 
         }
+
         if (Name == "GJ_button_04.png") {
              return secret
                 ? "geode.loader/GE_button_03.png" 
                 : (geode_theme ? "geode.loader/GE_button_03.png" : "GJ_button_04.png");
         }
+        
         if (Name == "bigFont.fnt") {
              return secret
                 ? "goldFont.fnt" 
@@ -25,7 +27,6 @@ namespace SpritePicker {
                 ? "geode.loader/GE_button_04.png" 
                 : (geode_theme ? "geode.loader/GE_button_05.png" : "GJ_button_01.png"); 
         };
-
 
         return ""; // Default return if no match
         }
--- src/SettingsV3/popup.hpp
+++ src/SettingsV3/popup.hpp
@@ -4,81 +4,146 @@
 #include "popup_nodes/CCIndexPackNode.hpp"
 #include "SelectionEnum.hpp"
 #include "../ButtonSprites/Sprite.hpp"
+#include <regex>
 using namespace geode::prelude;
 
 class Select : public geode::Popup<> {
 protected:
     bool m_theme = false;
+    double m_minsize;
     std::function<void(std::string)> m_settings;
     ScrollLayer* scroll;
-    CCNode* Item(auto send, auto modid, bool meme) {
-        return CCIndexPackNode::create(send,[=]() {
+    TextInput* searchBar;
+
+    CCIndexPackNode* Item(auto send, auto modid, bool meme) {
+        return CCIndexPackNode::create(send, [=]() {
             m_settings(modid);
             this->onClose(nullptr);
-        },m_theme
-        );
+        }, m_theme);
     }
+
     void closebtntheme(bool theme) {
         m_theme = theme;
         if (theme) {
             this->setCloseButtonSpr(
-                    CircleButtonSprite::createWithSpriteFrameName(
-                        "geode.loader/close.png", .85f,
-                            CircleBaseColor::DarkPurple 
-                    )
-                );
+                CircleButtonSprite::createWithSpriteFrameName(
+                    "geode.loader/close.png", .85f,
+                    CircleBaseColor::DarkPurple
+                )
+            );
         }
     }
+
     bool setup() {
         auto winSize = CCDirector::get()->getWinSize();
-		scroll = ScrollLayer::create(ccp(m_mainLayer->getContentSize().width / 1.5, m_mainLayer->getContentSize().height - 15));
+
+        scroll = ScrollLayer::create(ccp(
+            m_mainLayer->getContentSize().width - 30,
+            m_mainLayer->getContentSize().height - 70
+        ));
         scroll->setAnchorPoint(ccp(0, 0));
-        scroll->setPosition(ccp(88, 10 ));
+        scroll->setPosition(ccp(15, 15));
         m_mainLayer->addChild(scroll);
+
+        addSearchBar();
         return true;
-    };
+    }
+    void addSearchBar() {
+        auto winSize = m_mainLayer->getContentSize();
+
+        auto searchBarBg = CCLayerColor::create(ccc4(0, 0, 0, 100), 210.f, 35.f);
+        searchBarBg->setAnchorPoint(ccp(0.5f, 1));
+        searchBarBg->setPosition(ccp(winSize.width / 2, winSize.height - 50));
+        m_mainLayer->addChild(searchBarBg, -1);
+        searchBar = TextInput::create(
+            winSize.width / 2, "Search...", "bigFont.fnt"
+        );
+
+        searchBar->setCallback([=](std::string input){
+            filterItems(input);
+        });
+
+        searchBar->setAnchorPoint(ccp(0.5f, 1));
+        searchBar->setPosition(ccp(winSize.width / 2, winSize.height - 15));
+        m_mainLayer->addChild(searchBar);
+
+        auto divider = CCLayerColor::create(ccc4(200, 200, 200, 100), winSize.width - 30, 2);
+        divider->setAnchorPoint(ccp(0, 1));
+        divider->setPosition(ccp(15, winSize.height - 50));
+        m_mainLayer->addChild(divider);
+    }
+
+    void filterItems(const std::string& query) {
+        auto NodeScroller = scroll->m_contentLayer;
+        CCArrayExt<CCNode*> objects = NodeScroller->getChildren();
+        double maxcount =0 ;
+        for (auto* obj : objects) {
+            if (CCIndexPackNode* Cell = typeinfo_cast<CCIndexPackNode*>(obj)) {
+                std::regex pattern(query, std::regex::icase);
+                bool matches =  std::regex_search(Cell->getName(), pattern);
+                obj->setVisible(matches);
+            }
+             if (obj->isVisible()) maxcount+=1 ;
+        }
+        int basePosY = 180;
+        int i = -1;
+        float height = std::max<float>(m_minsize, 40 * maxcount);
+        for (auto* obj : objects) {
+            if (obj->isVisible()) {
+                i++;
+                obj->setPositionY(height - (40 * i));
+            } else {obj->setPositionY(-10000);}
+        }
+        NodeScroller->setContentSize(ccp(NodeScroller->getContentSize().width, height));
+        scroll->moveToTop();
+    }
+
+
     bool CreateWithArgs(bool meme, bool clicksound, std::function<void(std::string)> setting) {
-        CCNode* NodeScroller = scroll->m_contentLayer;
+        auto NodeScroller = scroll->m_contentLayer;
         m_settings = setting;
-        int basePosY = 207;
+        int basePosY = 180;
+
         auto json = (meme) ? ClickJson->memeData : ClickJson->usefulData;
-            for (const auto& [filename, data] : json) {
-                 if (data.clicks.empty() && clicksound) {
-                    continue;
-                 };
-                if (data.releases.empty() && !clicksound) {
-                    continue;
-                 };
-                CCNode* Object = Item(data,filename,clicksound);
-                Object->setPositionY(basePosY);
-                NodeScroller->addChild(Object);
+        for (const auto& [filename, data] : json) {
+            if (data.clicks.empty() && clicksound) {
+                continue;
+            }
+            if (data.releases.empty() && !clicksound) {
+                continue;
             }
 
-        float height = std::max<float>(scroll->getContentSize().height, 35 * NodeScroller->getChildrenCount());
+            auto Object = Item(data, filename, clicksound);
+            Object->setPositionY(basePosY);
+            NodeScroller->addChild(Object);
+        }
+        m_minsize = scroll->getContentSize().height;
+        float height = std::max<float>(m_minsize, 40 * NodeScroller->getChildrenCount());
         NodeScroller->setContentSize(ccp(NodeScroller->getContentSize().width, height));
         CCArrayExt<CCNode*> objects = NodeScroller->getChildren();
         int i = -1;
         for (auto* obj : objects) {
-                i++;
-                obj->setPositionY(height - (35 * i));
-            }
+            i++;
+            obj->setPositionY(height - (40 * i));
+        }
         scroll->moveToTop();
 
         return true;
-    };
+    }
+
 public:
-    static Select* create(bool meme = false, bool clicksound = true, std::function<void(std::string)> setting = [](std::string x) {},bool theme = false) {
+    static Select* create(bool meme = false, bool clicksound = true, std::function<void(std::string)> setting = [](std::string x) {}, bool theme = false) {
         auto ret = new Select;
-        
-        if (ret && ret->initAnchored(420.f, 210.f,SpritePicker::get("GJ_square01.png",theme))) {
+
+        if (ret && ret->initAnchored(420.f, 270.f, SpritePicker::get("GJ_square01.png", theme))) {
             ret->autorelease();
             ret->closebtntheme(theme);
-            ret->CreateWithArgs(meme,clicksound,setting);
+            ret->CreateWithArgs(meme, clicksound, setting);
             ret->setID("SoundSelector"_spr);
             return ret;
         }
 
         CC_SAFE_DELETE(ret);
         return nullptr;
-    };
+    }
 };
