f986907699e0b6637fa06ea16d39e5a4b84515ed
BeatACVR <75857504+BeatACVR@users.noreply.github.com>
2025-01-04 22:00:10 -0600

    2.0.5 release

--- src/main.cpp
+++ src/main.cpp
@@ -175,7 +175,7 @@ public:
 
 class $modify(MenuLayer) {
     void SendRequestAPI() {
-        if (Mod::get()->getSettingValue<bool>("offline-mode")) {
+        if (Mod::get()->getSavedValue<bool>("offlineMode")) {
             indexzip.Failed = true;
             indexzip.Finished = true;
             return;
