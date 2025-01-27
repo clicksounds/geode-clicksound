d08076fc8b8cba1fa43d882a961141d9cdd0b804
Viper <momo2010sofia@gmail.com>
2025-01-27 23:21:15 +0000

    push update

--- src/SettingsV3/PlaySound.hpp
+++ src/SettingsV3/PlaySound.hpp
@@ -1,127 +1,125 @@
-#include <Geode/loader/SettingV3.hpp>
-#include <Geode/loader/Mod.hpp>
-#include <Geode/binding/ButtonSprite.hpp>
 #include "../StaticClasses.hpp"
+#include <Geode/binding/ButtonSprite.hpp>
+#include <Geode/loader/Mod.hpp>
+#include <Geode/loader/SettingV3.hpp>
 
 using namespace geode::prelude;
 
 class MyButtonSettingV3 : public SettingV3 {
-public:
-    bool clicksound = false;
-    static Result<std::shared_ptr<SettingV3>> parse(std::string const& key, std::string const& modID, matjson::Value const& json) {
-        auto res = std::make_shared<MyButtonSettingV3>();
-        auto root = checkJson(json, "ClickSoundEffect");
-        res->init(key, modID, root);
-        root.has("clicksound").into(res->clicksound);
-        res->parseNameAndDescription(root);
-        res->parseEnableIf(root);
-        root.checkUnknownKeys();
-        return root.ok(std::static_pointer_cast<SettingV3>(res));
-    }
+  public:
+	bool clicksound = false;
+	static Result<std::shared_ptr<SettingV3>> parse(std::string const &key, std::string const &modID, matjson::Value const &json) {
+		auto res = std::make_shared<MyButtonSettingV3>();
+		auto root = checkJson(json, "ClickSoundEffect");
+		res->init(key, modID, root);
+		root.has("clicksound").into(res->clicksound);
+		res->parseNameAndDescription(root);
+		res->parseEnableIf(root);
+		root.checkUnknownKeys();
+		return root.ok(std::static_pointer_cast<SettingV3>(res));
+	}
 
-    bool load(matjson::Value const& json) override {
-        return true;
-    }
-    bool save(matjson::Value& json) const override {
-        return true;
-    }
+	bool load(matjson::Value const &json) override {
+		return true;
+	}
+	bool save(matjson::Value &json) const override {
+		return true;
+	}
 
-    bool isDefaultValue() const override {
-        return true;
-    }
+	bool isDefaultValue() const override {
+		return true;
+	}
 
-    void reset() override {}
+	void reset() override {}
 
-    SettingNodeV3* createNode(float width) override;
+	SettingNodeV3 *createNode(float width) override;
 };
 
 class MyButtonSettingNodeV3 : public SettingNodeV3 {
-protected:
-    CCSprite* m_buttonSprite;
-    CCMenuItemSpriteExtra* m_button;
-    std::shared_ptr<MyButtonSettingV3> m_setting;
+  protected:
+	CCSprite *m_buttonSprite;
+	CCMenuItemSpriteExtra *m_button;
+	std::shared_ptr<MyButtonSettingV3> m_setting;
 
-    bool init(std::shared_ptr<MyButtonSettingV3> setting, float width) {
-        if (!SettingNodeV3::init(setting, width))
-            return false;
-        
-        m_buttonSprite = CCSprite::createWithSpriteFrameName("GJ_playMusicBtn_001.png");
-        m_buttonSprite->setScale(.68f);
-        m_button = CCMenuItemSpriteExtra::create(
-            m_buttonSprite, this, menu_selector(MyButtonSettingNodeV3::onButton)
-        );
-        m_setting = setting;
-        this->getButtonMenu()->addChildAtPosition(m_button, Anchor::Center);
-        this->getButtonMenu()->setContentWidth(30);
-        this->getButtonMenu()->updateLayout();
+	bool init(std::shared_ptr<MyButtonSettingV3> setting, float width) {
+		if (!SettingNodeV3::init(setting, width))
+			return false;
 
-        this->updateState(nullptr);
-        
-        return true;
-    }
-    
-    void updateState(CCNode* invoker) override {
-        SettingNodeV3::updateState(invoker);
+		m_buttonSprite = CCSprite::createWithSpriteFrameName("GJ_playMusicBtn_001.png");
+		m_buttonSprite->setScale(.68f);
+		m_button = CCMenuItemSpriteExtra::create(
+		    m_buttonSprite, this, menu_selector(MyButtonSettingNodeV3::onButton));
+		m_setting = setting;
+		this->getButtonMenu()->addChildAtPosition(m_button, Anchor::Center);
+		this->getButtonMenu()->setContentWidth(30);
+		this->getButtonMenu()->updateLayout();
 
-        auto shouldEnable = this->getSetting()->shouldEnable();
-        m_button->setEnabled(shouldEnable);
-        m_buttonSprite->setCascadeColorEnabled(true);
-        m_buttonSprite->setCascadeOpacityEnabled(true);
-        m_buttonSprite->setOpacity(shouldEnable ? 255 : 155);
-        m_buttonSprite->setColor(shouldEnable ? ccWHITE : ccGRAY);
-    }
-    void onButton(CCObject*) {
-        if (m_setting->clicksound) {
-            if (Custom_OnClick) {
-                ClickSound->Play();
-            } else {
-                ClickSoundIndex->PlayRandom();
-            }
-        } else {
-        if (Custom_OnLetGo) {
-            ReleaseSound->Play();
-        } else {
-            ReleaseSoundIndex->PlayRandom();
-        }
-        }
-    }
+		this->updateState(nullptr);
 
-    void onCommit() override {}
-    void onResetToDefault() override {}
+		return true;
+	}
 
-public:
-    static MyButtonSettingNodeV3* create(std::shared_ptr<MyButtonSettingV3> setting, float width) {
-        auto ret = new MyButtonSettingNodeV3();
-        if (ret && ret->init(setting, width)) {
-            ret->autorelease();
-            return ret;
-        }
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
+	void updateState(CCNode *invoker) override {
+		SettingNodeV3::updateState(invoker);
 
-    bool hasUncommittedChanges() const override {
-        return false;
-    }
-    bool hasNonDefaultValue() const override {
-        return false;
-    }
+		auto shouldEnable = this->getSetting()->shouldEnable();
+		m_button->setEnabled(shouldEnable);
+		m_buttonSprite->setCascadeColorEnabled(true);
+		m_buttonSprite->setCascadeOpacityEnabled(true);
+		m_buttonSprite->setOpacity(shouldEnable ? 255 : 155);
+		m_buttonSprite->setColor(shouldEnable ? ccWHITE : ccGRAY);
+	}
+	void onButton(CCObject *) {
+		if (m_setting->clicksound) {
+			if (Custom_OnClick) {
+				ClickSound->Play();
+			} else {
+				ClickSoundIndex->PlayRandom();
+			}
+		} else {
+			if (Custom_OnLetGo) {
+				ReleaseSound->Play();
+			} else {
+				ReleaseSoundIndex->PlayRandom();
+			}
+		}
+	}
 
-    std::shared_ptr<MyButtonSettingV3> getSetting() const {
-        return std::static_pointer_cast<MyButtonSettingV3>(SettingNodeV3::getSetting());
-    }
+	void onCommit() override {}
+	void onResetToDefault() override {}
+
+  public:
+	static MyButtonSettingNodeV3 *create(std::shared_ptr<MyButtonSettingV3> setting, float width) {
+		auto ret = new MyButtonSettingNodeV3();
+		if (ret && ret->init(setting, width)) {
+			ret->autorelease();
+			return ret;
+		}
+		CC_SAFE_DELETE(ret);
+		return nullptr;
+	}
+
+	bool hasUncommittedChanges() const override {
+		return false;
+	}
+	bool hasNonDefaultValue() const override {
+		return false;
+	}
+
+	std::shared_ptr<MyButtonSettingV3> getSetting() const {
+		return std::static_pointer_cast<MyButtonSettingV3>(SettingNodeV3::getSetting());
+	}
 };
 
-SettingNodeV3* MyButtonSettingV3::createNode(float width) {
-    return MyButtonSettingNodeV3::create(
-        std::static_pointer_cast<MyButtonSettingV3>(shared_from_this()),
-        width
-    );
+SettingNodeV3 *MyButtonSettingV3::createNode(float width) {
+	return MyButtonSettingNodeV3::create(
+	    std::static_pointer_cast<MyButtonSettingV3>(shared_from_this()),
+	    width);
 }
 
 $execute {
-   auto ret = Mod::get()->registerCustomSettingType("clicksoundeffect", &MyButtonSettingV3::parse);
-    if (!ret) {
-        log::error("Unable to register setting type: {}", ret.unwrapErr());
-    }
+	auto ret = Mod::get()->registerCustomSettingType("clicksoundeffect", &MyButtonSettingV3::parse);
+	if (!ret) {
+		log::error("Unable to register setting type: {}", ret.unwrapErr());
+	}
 }
--- src/StaticClasses.hpp
+++ src/StaticClasses.hpp
@@ -1,123 +1,122 @@
 #pragma once
-#include <Geode/Geode.hpp>
 #include "jsonReader/Getsettingsinfo.hpp"
+#include <Geode/Geode.hpp>
 
 static bool Custom_OnClick = false;
 static bool Custom_OnLetGo = false;
 
 struct downloadedzipStruc {
-    bool Finished = false;
-    bool Failed = false;
-    bool StartedDownloading = false;
+	bool Finished = false;
+	bool Failed = false;
+	bool StartedDownloading = false;
 };
 
 static downloadedzipStruc indexzip;
-FMOD::ChannelGroup* CS_Group; 
-FMOD::DSP* pitchShifterDSP;  
+FMOD::ChannelGroup *CS_Group;
+FMOD::DSP *pitchShifterDSP;
 using namespace geode::prelude;
 // Custom class for Caching sounds (Make it less laggy for mobile platforms and such)
 class SoundCache {
-public:
-    std::string m_soundFile = "";
-    FMOD::Sound* m_sound;
-    std::string Volume;
-    std::string custom;   
-    FMOD::Channel* Soundchannel;
-    
-    SoundCache(std::string x, std::string x2) : Volume(x), custom(x2), Soundchannel(nullptr) {
-        if (!CS_Group) {
-            FMODAudioEngine::sharedEngine()->m_system->createChannelGroup("CS_Group", &CS_Group);
-        }
+  public:
+	std::string m_soundFile = "";
+	FMOD::Sound *m_sound;
+	std::string Volume;
+	std::string custom;
+	FMOD::Channel *Soundchannel;
 
-        if (!pitchShifterDSP) {
-            FMODAudioEngine::sharedEngine()->m_system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pitchShifterDSP);
-            CS_Group->addDSP(0, pitchShifterDSP);
-        }
-    }
+	SoundCache(std::string x, std::string x2) : Volume(x), custom(x2), Soundchannel(nullptr) {
+		if (!CS_Group) {
+			FMODAudioEngine::sharedEngine()->m_system->createChannelGroup("CS_Group", &CS_Group);
+		}
 
-    void Setsound(std::string soundFile) {
-        if (soundFile.c_str()) {
-            if (FMODAudioEngine::sharedEngine()->m_system->createSound(soundFile.c_str(), FMOD_DEFAULT, nullptr, &m_sound) == FMOD_OK) {
-                m_sound->setMode(FMOD_LOOP_OFF);
-                m_soundFile = soundFile;
-            }
-        } 
-    }
+		if (!pitchShifterDSP) {
+			FMODAudioEngine::sharedEngine()->m_system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pitchShifterDSP);
+			CS_Group->addDSP(0, pitchShifterDSP);
+		}
+	}
 
-    void Play(bool TestButton = false) { 
-        std::string Paths = GetSettingJsonRead(custom).Custom_Sound_Path;
-        if (m_soundFile != Paths) {
-            Setsound(Paths);
-        }
-        if (!m_sound) {
-            return;
-        }
-        PlayModded(TestButton);
-    }
+	void Setsound(std::string soundFile) {
+		if (soundFile.c_str()) {
+			if (FMODAudioEngine::sharedEngine()->m_system->createSound(soundFile.c_str(), FMOD_DEFAULT, nullptr, &m_sound) == FMOD_OK) {
+				m_sound->setMode(FMOD_LOOP_OFF);
+				m_soundFile = soundFile;
+			}
+		}
+	}
 
-    void PlayModded(bool TestButton = false) {
-        float GetVolume = Mod::get()->getSettingValue<int64_t>(Volume);
-        if (GetVolume <= 0 && TestButton == true) {
-            GetVolume = 1;
-        }
-        FMODAudioEngine::sharedEngine()->m_system->playSound(m_sound, CS_Group, false, &Soundchannel);
-        Soundchannel->setVolume(GetVolume / 50.f);
-        double semitone =  static_cast<double>(Mod::get()->getSettingValue<int64_t>("sfx-semitone")) / 12;
-        if (semitone < 0) {
-            semitone = std::pow(2,semitone); // fix negtive octave
-        } else {
-            semitone+=1;
-        }
-        pitchShifterDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, semitone); // semitone is half a octave
-    }
+	void Play(bool TestButton = false) {
+		std::string Paths = GetSettingJsonRead(custom).Custom_Sound_Path;
+		if (m_soundFile != Paths) {
+			Setsound(Paths);
+		}
+		if (!m_sound) {
+			return;
+		}
+		PlayModded(TestButton);
+	}
 
-    ~SoundCache() {
-        if (m_sound) {
-            m_sound->release();
-        }
-    }
+	void PlayModded(bool TestButton = false) {
+		float GetVolume = Mod::get()->getSettingValue<int64_t>(Volume);
+		if (GetVolume <= 0 && TestButton == true) {
+			GetVolume = 1;
+		}
+		FMODAudioEngine::sharedEngine()->m_system->playSound(m_sound, CS_Group, false, &Soundchannel);
+		Soundchannel->setVolume(GetVolume / 50.f);
+		double semitone = static_cast<double>(Mod::get()->getSettingValue<int64_t>("sfx-semitone")) / 12;
+		if (semitone < 0) {
+			semitone = std::pow(2, semitone); // fix negtive octave
+		} else {
+			semitone += 1;
+		}
+		pitchShifterDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, semitone); // semitone is half a octave
+	}
+
+	~SoundCache() {
+		if (m_sound) {
+			m_sound->release();
+		}
+	}
 };
-
 
 class MultiSoundCache {
-public:
-    std::vector<SoundCache*> m_sounds;
+  public:
+	std::vector<SoundCache *> m_sounds;
 
-    MultiSoundCache() {
-        std::srand(static_cast<unsigned>(std::time(nullptr)));
-    }
+	MultiSoundCache() {
+		std::srand(static_cast<unsigned>(std::time(nullptr)));
+	}
 
-    void SetSounds(const std::vector<std::string>& soundFiles, const std::string& volume, const std::string& custom) {
-        std::vector<SoundCache*> memsafe = m_sounds;
-        m_sounds.clear();
-        for (auto& sound : memsafe) {
-            delete sound;
-        }
-        memsafe.clear(); 
-        for (const auto& soundFile : soundFiles) {
-            SoundCache* sound = new SoundCache(volume, custom);
-            sound->Setsound(soundFile);
-            m_sounds.push_back(sound);
-        }
-    }
+	void SetSounds(const std::vector<std::string> &soundFiles, const std::string &volume, const std::string &custom) {
+		std::vector<SoundCache *> memsafe = m_sounds;
+		m_sounds.clear();
+		for (auto &sound : memsafe) {
+			delete sound;
+		}
+		memsafe.clear();
+		for (const auto &soundFile : soundFiles) {
+			SoundCache *sound = new SoundCache(volume, custom);
+			sound->Setsound(soundFile);
+			m_sounds.push_back(sound);
+		}
+	}
 
-    void PlayRandom() {
-        if (!m_sounds.empty()) {
-            int randomIndex = std::rand() % m_sounds.size();
-            m_sounds[randomIndex]->PlayModded();
-        }
-    }
+	void PlayRandom() {
+		if (!m_sounds.empty()) {
+			int randomIndex = std::rand() % m_sounds.size();
+			m_sounds[randomIndex]->PlayModded();
+		}
+	}
 
-    ~MultiSoundCache() {
-        for (auto& sound : m_sounds) {
-            delete sound;
-        }
-    }
+	~MultiSoundCache() {
+		for (auto &sound : m_sounds) {
+			delete sound;
+		}
+	}
 };
 
-static MultiSoundCache* ClickSoundIndex = new MultiSoundCache();
-static MultiSoundCache* ReleaseSoundIndex = new MultiSoundCache();
+static MultiSoundCache *ClickSoundIndex = new MultiSoundCache();
+static MultiSoundCache *ReleaseSoundIndex = new MultiSoundCache();
 
 // Create the classes for Caching
-static SoundCache* ClickSound = new SoundCache("click-volume","selection-clicks");
-static SoundCache* ReleaseSound = new SoundCache("release-volume","selection-release");
+static SoundCache *ClickSound = new SoundCache("click-volume", "selection-clicks");
+static SoundCache *ReleaseSound = new SoundCache("release-volume", "selection-release");
