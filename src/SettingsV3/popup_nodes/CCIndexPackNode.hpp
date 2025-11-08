5774ca0f79a10076326404c84649a958f6eee856
BeatACVR <75857504+BeatACVR@users.noreply.github.com>
2025-11-07 20:02:30 -0600

    revert to v2.1.0

--- src/SettingsV3/popup_nodes/CCIndexPackNode.hpp
+++ src/SettingsV3/popup_nodes/CCIndexPackNode.hpp
@@ -14,8 +14,6 @@
 #include <iostream>
 using namespace geode::prelude;
 
-extern void onsettingsUpdate();
-
 float clampf_f(float value, float min, float max) {
 	if (value < min)
 		return min;
@@ -70,7 +68,6 @@ class CCIndexPackNode : public CCLayerColor {
 	CCMenu *DEVS;
 	std::string authorsListWhole = "";
 	std::string packDescription;
-	bool isClickSection = true; // Add a flag to indicate if this node is for clicks or releases
 	void OnDevelopers(auto sender) {
 		MDPopup::create(
 		    "More Info",
@@ -127,12 +124,11 @@ class CCIndexPackNode : public CCLayerColor {
 			}
 		}
 	}
-	bool init(CategoryData x, std::function<void()> Objectt, bool theme, bool isClick = true) {
+	bool init(CategoryData x, std::function<void()> Objectt, bool theme) {
 		if (!CCLayerColor::init())
 			return false;
 		selectionobject = Objectt;
 		Infomation = x;
-		isClickSection = isClick;
 
 		this->setContentSize(ccp(390, 35));
 		this->setAnchorPoint(ccp(0, 1));
@@ -243,40 +239,25 @@ class CCIndexPackNode : public CCLayerColor {
 		this->setOpacity(0);
 		// GJ_button_06
 		auto ConfirmSprite = CCMenuItemSpriteExtra::create(ButtonSprite::create("Set", 40.f, true, SpritePicker::get("bigFont.fnt", theme), SpritePicker::get("GJ_button_01.png", theme), 20.f, 1.0f), this, menu_selector(CCIndexPackNode::selected));
-        ConfirmSprite->m_scaleMultiplier = 0.9;
-        MEN(_Apply_Menu)
-        _Apply_Menu->setID("apply");
-        _Apply_Menu->ignoreAnchorPointForPosition(false);
-        float spacing = 8.0f;
-        float totalWidth = ConfirmSprite->getContentSize().width;
-	//CCMenuItemSpriteExtra* BothSprite = nullptr;
-	// Use parsed CategoryData vectors (populated by JsonReader) to determine availability
-	if (!Infomation.clicks.empty() && !Infomation.releases.empty()) {
-            /*BothSprite = CCMenuItemSpriteExtra::create(
-                ButtonSprite::create("Both", 40.f, true, SpritePicker::get("bigFont.fnt", theme), SpritePicker::get("GJ_button_01.png", theme), 20.f, 1.0f),
-                this, menu_selector(CCIndexPackNode::selectedBoth));
-            BothSprite->m_scaleMultiplier = 0.9;
-            totalWidth += BothSprite->getContentSize().width + spacing;*/
-        }
-        _Apply_Menu->setContentSize(CCSize(totalWidth, ConfirmSprite->getContentSize().height));
-        ConfirmSprite->setAnchorPoint({1, 0.5f});
-        ConfirmSprite->setPosition(totalWidth, ConfirmSprite->getContentSize().height / 2);
-        _Apply_Menu->addChild(ConfirmSprite);
-        /*if (BothSprite) {
-            BothSprite->setAnchorPoint({1, 0.5f});
-            BothSprite->setPosition(totalWidth - ConfirmSprite->getContentSize().width - spacing, BothSprite->getContentSize().height / 2);
-            _Apply_Menu->addChild(BothSprite);
-        }*/
-        _Apply_Menu->setPosition(ccp(this->getContentSize().width, this->getContentSize().height / 2));
-        _Apply_Menu->updateLayout();
-        this->addChild(_Apply_Menu);
-        _Apply_Menu->setAnchorPoint({1.0, 0.5});
-
+		ConfirmSprite->m_scaleMultiplier = 0.9;
+		MEN(_Apply_Menu)
+		_Apply_Menu->setID("apply");
+		_Apply_Menu->ignoreAnchorPointForPosition(false);
+		_Apply_Menu->addChild(ConfirmSprite);
+		_Apply_Menu->setLayout(RowLayout::create()
+		                           ->setAxisAlignment(AxisAlignment::Start)
+		                           ->setCrossAxisLineAlignment(AxisAlignment::Start)
+		                           ->setCrossAxisAlignment(AxisAlignment::Start));
+		_Apply_Menu->setContentSize(ConfirmSprite->getContentSize());
+		_Apply_Menu->setPosition(ccp(this->getContentSize().width - ConfirmSprite->getContentSize().width / 1.5, this->getContentSize().height / 2));
+		_Apply_Menu->updateLayout();
+		this->addChild(_Apply_Menu);
+		_Apply_Menu->setAnchorPoint({0.5, 0.5});
 		return true;
 	}
-	static CCIndexPackNode *create(CategoryData x, std::function<void()> Objectt, bool theme, bool isClick = true) {
+	static CCIndexPackNode *create(CategoryData x, std::function<void()> Objectt, bool theme) {
 		CCIndexPackNode *pRet = new CCIndexPackNode();
-		if (pRet && pRet->init(x, Objectt, theme, isClick)) {
+		if (pRet && pRet->init(x, Objectt, theme)) {
 			pRet->autorelease();
 			return pRet;
 		} else {
@@ -284,90 +265,4 @@ class CCIndexPackNode : public CCLayerColor {
 			return nullptr;
 		}
 	};
-	/*bool hasClickAndReleaseSounds(const std::string& folderPath) {
-		std::filesystem::path dirPath = folderPath;
-		// If a file (likely pack.json) was passed, use its parent folder
-		if (std::filesystem::is_regular_file(dirPath)) {
-			dirPath = dirPath.parent_path();
-		}
-
-		if (!std::filesystem::is_directory(dirPath)) {
-			geode::log::info("hasClickAndReleaseSounds: path is not a directory: {}", dirPath.string());
-			return false;
-		}
-
-		bool hasClick = false;
-		bool hasRelease = false;
-
-		// Look for Clicks and Releases subfolders (case-insensitive)
-		for (const auto& entry : std::filesystem::directory_iterator(dirPath)) {
-			if (!entry.is_directory()) continue;
-			std::string folder = entry.path().filename().string();
-			std::string lower = folder;
-			std::transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
-			if (lower == "clicks") {
-				for (const auto& f : std::filesystem::directory_iterator(entry.path())) {
-					if (f.is_regular_file()) {
-						hasClick = true;
-						break;
-					}
-				}
-			} else if (lower == "releases") {
-				for (const auto& f : std::filesystem::directory_iterator(entry.path())) {
-					if (f.is_regular_file()) {
-						hasRelease = true;
-						break;
-					}
-				}
-			}
-			if (hasClick && hasRelease) {
-				geode::log::info("hasClickAndReleaseSounds: both Clicks and Releases found in {}", dirPath.string());
-				return true;
-			}
-		}
-
-		if (!hasClick) geode::log::info("hasClickAndReleaseSounds: no Click files in {}", dirPath.string());
-		if (!hasRelease) geode::log::info("hasClickAndReleaseSounds: no Release files in {}", dirPath.string());
-		return false;
-	}
-	void selectedBoth(CCObject *) {
-		try {
-			geode::log::info("Both button pressed");
-			bool isMeme = false;
-			geode::log::info("isClickSection: {}", isClickSection);
-			geode::log::info("Infomation.clicks.size(): {} Infomation.releases.size(): {}", Infomation.clicks.size(), Infomation.releases.size());
-			if (!isClickSection && Infomation.clicks.empty() && !Infomation.releases.empty()) {
-				isMeme = true;
-			}
-			geode::log::info("Determined isMeme: {}", isMeme);
-			std::string packId = m_name;
-			geode::log::info("PackId: {}", packId);
-			for (const char* key : {"selection-clicks", "selection-release"}) {
-				geode::log::info("Updating setting: {}", key);
-				std::string valStr = Mod::get()->getSettingValue<std::string>(key);
-				geode::log::info("Old value: {}", valStr);
-				matjson::Value val = matjson::parse(valStr).unwrapOrDefault();
-				if (isMeme) {
-					val["Current_Sound_Meme"] = packId;
-					val["Tab"] = 0;
-					geode::log::info("Set Current_Sound_Meme to {} and Tab to 0", packId);
-				} else {
-					val["Current_Sound_Useful"] = packId;
-					val["Tab"] = 1;
-					geode::log::info("Set Current_Sound_Useful to {} and Tab to 1", packId);
-				}
-				std::string newVal = val.dump(matjson::NO_INDENTATION);
-				geode::log::info("New value: {}", newVal);
-				Mod::get()->setSettingValue(key, newVal);
-				onsettingsUpdate();
-			}
-			if (selectionobject) {
-				geode::log::info("Calling selectionobject() to close popup");
-				selectionobject();
-			}
-			geode::log::info("Both button logic complete");
-		} catch (...) {
-			geode::log::error("Failed to set both click and release pack");
-		}
-	}*/
 };
--- src/StaticClasses.hpp
+++ src/StaticClasses.hpp
@@ -2,144 +2,121 @@
 #include "jsonReader/Getsettingsinfo.hpp"
 #include <Geode/Geode.hpp>
 
-using namespace geode::prelude;
-
-static FMOD::ChannelGroup* SFX_Group = nullptr;
-
-struct downloadedzipStruc {
-    bool Finished = false;
-    bool Failed = false;
-    bool StartedDownloading = false;
-};
-
-static downloadedzipStruc indexzip;
-
 static bool Custom_OnClick = false;
 static bool Custom_OnLetGo = false;
 
+struct downloadedzipStruc {
+	bool Finished = false;
+	bool Failed = false;
+	bool StartedDownloading = false;
+};
+
+static downloadedzipStruc indexzip;
+FMOD::ChannelGroup *CS_Group;
+FMOD::DSP *pitchShifterDSP;
+using namespace geode::prelude;
+// Custom class for Caching sounds (Make it less laggy for mobile platforms and such)
 class SoundCache {
-public:
-    std::string m_soundFile = "";
-    FMOD::Sound* m_sound = nullptr;
-    std::string Volume;
-    std::string custom;
+  public:
+	std::string m_soundFile = "";
+	FMOD::Sound *m_sound;
+	std::string Volume;
+	std::string custom;
+	FMOD::Channel *Soundchannel;
 
-    SoundCache(std::string vol, std::string cust)
-        : Volume(vol), custom(cust) {
-        if (!SFX_Group) {
-            FMODAudioEngine::sharedEngine()->m_system->createChannelGroup("SFX_Group", &SFX_Group);
-            // attach to master, but never to music
-            FMOD::ChannelGroup* master = nullptr;
-            FMODAudioEngine::sharedEngine()->m_system->getMasterChannelGroup(&master);
-            if (master) {
-                master->addGroup(SFX_Group);
-            }
-        }
-    }
-
-    void Setsound(const std::string& soundFile) {
-        if (!soundFile.empty()) {
-            if (FMODAudioEngine::sharedEngine()->m_system->createSound(
-                soundFile.c_str(), FMOD_DEFAULT, nullptr, &m_sound) == FMOD_OK) {
-                m_sound->setMode(FMOD_LOOP_OFF);
-                m_soundFile = soundFile;
-            }
-        }
-    }
-
-    void Play(bool TestButton = false) {
-        std::string Paths = GetSettingJsonRead(custom).Custom_Sound_Path;
-        if (m_soundFile != Paths) {
-            Setsound(Paths);
-        }
-        if (!m_sound) return;
-        PlayModded(TestButton);
-    }
-
-
-	void PlayModded(bool TestButton = false) {
-		float GetVolume = Mod::get()->getSettingValue<int64_t>(Volume);
-		if (GetVolume <= 0 && TestButton) GetVolume = 1;
-		if (!m_sound) return;
-
-		FMOD::Channel* channel = nullptr;
-
-		const int MAX_SFX = 100;
-		int active = 0;
-		if (SFX_Group) SFX_Group->getNumChannels(&active);
-		if (active >= MAX_SFX) {
-			int idx = rand() % active;
-			FMOD::Channel* old = nullptr;
-			SFX_Group->getChannel(idx, &old);
-			if (old) old->stop();
-			active--;
+	SoundCache(std::string x, std::string x2) : Volume(x), custom(x2), Soundchannel(nullptr) {
+		if (!CS_Group) {
+			FMODAudioEngine::sharedEngine()->m_system->createChannelGroup("CS_Group", &CS_Group);
 		}
 
-		FMODAudioEngine::sharedEngine()->m_system->playSound(m_sound, SFX_Group, false, &channel);
-		if (!channel) return;
-
-		float scale = 1.0f / std::sqrt(static_cast<float>(active+1)); 
-		float randScale = 0.85f + static_cast<float>(rand()) / RAND_MAX * 0.3f;
-		channel->setVolume((GetVolume / 50.f) * scale * randScale);
-
-		FMOD::DSP* localPitchDSP = nullptr;
-		FMODAudioEngine::sharedEngine()->m_system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &localPitchDSP);
-
-		double semitone = static_cast<double>(Mod::get()->getSettingValue<int64_t>("sfx-semitone")) / 12.0;
-		if (semitone < 0) semitone = std::pow(2.0, semitone);
-		else semitone += 1.0;
-
-		if (localPitchDSP) {
-			localPitchDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, static_cast<float>(semitone));
-			channel->addDSP(0, localPitchDSP);
-			localPitchDSP->release();
+		if (!pitchShifterDSP) {
+			FMODAudioEngine::sharedEngine()->m_system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pitchShifterDSP);
+			CS_Group->addDSP(0, pitchShifterDSP);
 		}
 	}
 
+	void Setsound(std::string soundFile) {
+		if (soundFile.c_str()) {
+			if (FMODAudioEngine::sharedEngine()->m_system->createSound(soundFile.c_str(), FMOD_DEFAULT, nullptr, &m_sound) == FMOD_OK) {
+				m_sound->setMode(FMOD_LOOP_OFF);
+				m_soundFile = soundFile;
+			}
+		}
+	}
 
+	void Play(bool TestButton = false) {
+		std::string Paths = GetSettingJsonRead(custom).Custom_Sound_Path;
+		if (m_soundFile != Paths) {
+			Setsound(Paths);
+		}
+		if (!m_sound) {
+			return;
+		}
+		PlayModded(TestButton);
+	}
 
-    ~SoundCache() {
-        if (m_sound) {
-            m_sound->release();
-        }
-    }
+	void PlayModded(bool TestButton = false) {
+		float GetVolume = Mod::get()->getSettingValue<int64_t>(Volume);
+		if (GetVolume <= 0 && TestButton == true) {
+			GetVolume = 1;
+		}
+		FMODAudioEngine::sharedEngine()->m_system->playSound(m_sound, CS_Group, false, &Soundchannel);
+		Soundchannel->setVolume(GetVolume / 50.f);
+		double semitone = static_cast<double>(Mod::get()->getSettingValue<int64_t>("sfx-semitone")) / 12;
+		if (semitone < 0) {
+			semitone = std::pow(2, semitone); // fix negtive octave
+		} else {
+			semitone += 1;
+		}
+		pitchShifterDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, semitone); // semitone is half a octave
+	}
+
+	~SoundCache() {
+		if (m_sound) {
+			m_sound->release();
+		}
+	}
 };
 
 class MultiSoundCache {
-public:
-    std::vector<SoundCache*> m_sounds;
+  public:
+	std::vector<SoundCache *> m_sounds;
 
-    MultiSoundCache() {
-        std::srand(static_cast<unsigned>(std::time(nullptr)));
-    }
+	MultiSoundCache() {
+		std::srand(static_cast<unsigned>(std::time(nullptr)));
+	}
 
-    void SetSounds(const std::vector<std::string>& soundFiles, const std::string& volume, const std::string& custom) {
-        for (auto& sound : m_sounds) {
-            delete sound;
-        }
-        m_sounds.clear();
-        for (const auto& soundFile : soundFiles) {
-            SoundCache* sound = new SoundCache(volume, custom);
-            sound->Setsound(soundFile);
-            m_sounds.push_back(sound);
-        }
-    }
+	void SetSounds(const std::vector<std::string> &soundFiles, const std::string &volume, const std::string &custom) {
+		std::vector<SoundCache *> memsafe = m_sounds;
+		m_sounds.clear();
+		for (auto &sound : memsafe) {
+			delete sound;
+		}
+		memsafe.clear();
+		for (const auto &soundFile : soundFiles) {
+			SoundCache *sound = new SoundCache(volume, custom);
+			sound->Setsound(soundFile);
+			m_sounds.push_back(sound);
+		}
+	}
 
-    void PlayRandom() {
-        if (!m_sounds.empty()) {
-            int randomIndex = std::rand() % m_sounds.size();
-            m_sounds[randomIndex]->PlayModded();
-        }
-    }
+	void PlayRandom() {
+		if (!m_sounds.empty()) {
+			int randomIndex = std::rand() % m_sounds.size();
+			m_sounds[randomIndex]->PlayModded();
+		}
+	}
 
-    ~MultiSoundCache() {
-        for (auto& sound : m_sounds) {
-            delete sound;
-        }
-    }
+	~MultiSoundCache() {
+		for (auto &sound : m_sounds) {
+			delete sound;
+		}
+	}
 };
 
-static MultiSoundCache* ClickSoundIndex = new MultiSoundCache();
-static MultiSoundCache* ReleaseSoundIndex = new MultiSoundCache();
-static SoundCache* ClickSound = new SoundCache("click-volume", "selection-clicks");
-static SoundCache* ReleaseSound = new SoundCache("release-volume", "selection-release");
+static MultiSoundCache *ClickSoundIndex = new MultiSoundCache();
+static MultiSoundCache *ReleaseSoundIndex = new MultiSoundCache();
+
+// Create the classes for Caching
+static SoundCache *ClickSound = new SoundCache("click-volume", "selection-clicks");
+static SoundCache *ReleaseSound = new SoundCache("release-volume", "selection-release");
